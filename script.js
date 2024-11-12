b194dddd58967f52156b56778e2f267498e00f63
americansinthebed <96220901+americansinthebed@users.noreply.github.com>
2024-11-12 14:03:52 +1100

    added website main page html & css

---
+++ script.js
@@ -0,0 +1,5 @@
+// add later
+document.querySelector('.cta-button').addEventListener('click', function (e) {
+    e.preventDefault();
+    document.querySelector('#collections').scrollIntoView({ behavior: 'smooth' });
+});
